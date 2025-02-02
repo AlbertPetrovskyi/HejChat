7f7d23eb052b3fa400dbb229be1fc5b68dc647f2
Albert Kavinsky <petrovskyia@gytool.cz>
2025-02-02 21:43:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	Telegram.WebApp.init();
 	const webApp = Telegram.WebApp;
 	webApp.ready();
+	webApp.expand();
 });
 
 function closeBot() {
